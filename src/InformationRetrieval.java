3f7fee8f7eb0e90edd02e8f8c7ebc866179e5ba2
darknight <icsd11175@icsd.aegean.gr>
2015-05-12 18:10:00 +0300

    Added basic runtime

--- src/InformationRetrieval.java
+++ src/InformationRetrieval.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,6 +18,9 @@ import java.util.logging.Logger;
  * @author Konstantinos Chasiotis
  */
 public class InformationRetrieval {
+
+    static String queries5SanitizedPath = "queries5Sanitized.txt";
+    static String queriesAllSanitizedPath = "queries5Sanitized.txt";
 
     public static ArrayList LoadQueries(String filepath, int database) {
         ArrayList<Query> Queries = new ArrayList();
@@ -43,6 +48,18 @@ public class InformationRetrieval {
         return Queries;
     }
 
+    private static void StemQueries(ArrayList<Query> queries) {
+        for (int i = 0; i < queries.size(); i++) {
+            queries.get(i).Description = new Stemmer().Run(queries.get(i).Description);
+        }
+    }
+
+    private static void RemoveStopwords(ArrayList<Query> queries) {
+        for (int i = 0; i < queries.size(); i++) {
+            queries.get(i).Description = new StopwordRemover().Run(queries.get(i).Description);
+        }
+    }
+
     /**
      * @param args the command line arguments
      */
@@ -52,16 +69,19 @@ public class InformationRetrieval {
         String queries5wordspath = "queries5words.txt";
 
         //Test database value set to 0
-        ArrayList<Query> queries5words = LoadQueries(queries5wordspath, 0);
-        ArrayList<Query> queriesallwords = LoadQueries(queriesallwordspath, 0);
+        ArrayList<Query> queries5wordsTest = LoadQueries(queries5wordspath, 0);
+        ArrayList<Query> queriesallwordsTest = LoadQueries(queriesallwordspath, 0);
 
-        System.out.println(queries5words);
-        //System.out.println("\n"+queries5words.get);
+        System.out.println(queriesallwordsTest + "\n");
+        //QueryToFile(queriesallwordsTest);
+        //System.out.println(new Stemmer().Run(new String[]{queries5SanitizedPath}) + "\n");
+        StemQueries(queriesallwordsTest);
+        System.out.println("stemmer \n" + queriesallwordsTest + "\n");
         //for (int i = 0; i < queries5words.size(); i++) {
         for (int i = 2; i <= 2; i++) {
-            queries5words.get(i).RunQuery();
+            queries5wordsTest.get(i).RunQuery();
             RetrievalModels temp = new RetrievalModels();
-            temp.myQuery = queries5words.get(i);
+            temp.myQuery = queries5wordsTest.get(i);
             /*temp.RunBooleanModel();
              System.out.println("Query: " + queries5words.get(i).QueryID + ", Boolean model retrieved: \n" + temp.BooleanList);
              temp.RunTF();
@@ -73,15 +93,55 @@ public class InformationRetrieval {
              System.out.println("Query: " + queries5words.get(i).QueryID + ", Okapi model retrieved: \n" + temp.B25_List);
              */
             temp.TF_IDFEval();
-            System.out.println("Query: " + queries5words.get(i).QueryID + ", Eval tfidf: \n" + temp.TF_IDFEval.toString());
+            System.out.println("Query: " + queries5wordsTest.get(i).QueryID + ", Eval tfidf: \n" + temp.TF_IDFEval.toString());
 
         }
 
-        /*queries5words.get(4).RunQuery();
-         RetrievalModels temp = new RetrievalModels();
-         temp.myQuery = queries5words.get(4);
-         temp.RunBooleanModel();
-         System.out.println("Retrieved: \n" + temp.BooleanList);*/
+        
+        
+        //<editor-fold defaultstate="collapsed" desc="Initialize queries">
+        for (int i = 0; i < 4; i++) {
+            ArrayList<Query> queries5words = LoadQueries(queries5wordspath, i);
+            ArrayList<Query> queriesallwords = LoadQueries(queriesallwordspath, i);
+
+            runAllModels(i, queries5words);
+            runAllModels(i, queriesallwords);
+        }
+        //</editor-fold>
     }
 
+    private static void runAllModels(int database, ArrayList<Query> queries) {
+
+        switch (database) {
+            case 0:
+                break;
+            case 1:
+                StemQueries(queries);
+                break;
+            case 2:
+                RemoveStopwords(queries);
+                break;
+            case 3:
+                StemQueries(queries);
+                RemoveStopwords(queries);
+                break;
+            default:
+                System.out.println("Error: Wrong database number");
+                break;
+
+        }
+
+        for (int i = 0; i < queries.size(); i++) {
+            queries.get(i).RunQuery();
+            RetrievalModels temp = new RetrievalModels();
+            temp.myQuery = queries.get(i);
+            temp.RunBooleanModel();
+            temp.RunTF();
+            temp.RunTF_IDF();
+            temp.RunB25();
+
+            temp.TF_IDFEval();
+            //Todo add more evaluation functions
+        }
+    }
 }
